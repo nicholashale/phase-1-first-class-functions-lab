8a17a77803c52b61bcc944b993d6e157058173d5
Nicholas Weinberg <nickweinberg@fastmail.com>
2022-03-15 11:20:52 -0700

    commit tree 4841a6ee22edf13eef45645d6513d445ad417fab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = (drivers) => {
+    return [drivers[0], drivers[1]];
+}
+
+const returnLastTwoDrivers = (drivers) => {
+    return [drivers[drivers.length - 2], drivers[drivers.length - 1]]
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier) {
+    const fareMultiplier = (fare) => fare * multiplier;
+    return fareMultiplier;
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, firstOrLastTwo) {
+    return firstOrLastTwo(drivers);
+}
